a4af2f8142f86199639ed6bd46da8869f8774179
Yuvrajsinh Borasiya <102581418+Yuvrajsinh2111@users.noreply.github.com>
2022-12-19 18:40:18 +0530

    Update index.tsx

--- services/index.tsx
+++ services/index.tsx
@@ -10,7 +10,6 @@ export const getLandingPageData = async () => {
         "Content-Length": "0",
         Cookie:
           "_ga=GA1.1.996028608.1671099854; _gid=GA1.1.1193542507.1671099854; searchLocation=^%^7B^%^7D; connect.sid=s^%^3Aojnm2J74pKUCuk-vTu1waIKqNyUfgjg1.7WznZRoCOdLk1Ngx6qdtTkwKQaO1ebQt7dqFeL7LZvI; lang=^%^7B^%^22lang^%^22^%^3A^%^22en^%^22^%^7D; io=4uDdtWkGGtko9DWZAAL3; _gat_gtag_UA_152424655_2=1; connect.sid=s%3A1k4NgngmuDouW7XPfkjW9YmRv5M6qCs7.dwSUSNAv7z3riYoCuIgYb6ciVrcUfYnpFMSPvYR9ISs",
-        Origin: "http://143.110.181.77:9044"
       },
     };
 
