3962a0f917efb0e03c5910d0f92657caf30cb614
Yuvrajsinh Borasiya <102581418+Yuvrajsinh2111@users.noreply.github.com>
2022-12-19 18:11:58 +0530

    Update index.tsx

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Container from "../container";
 import Footer from "../footer";
 import Navbar from "../navbar";
@@ -6,14 +5,6 @@ import Navbar from "../navbar";
 const Layout = ({ children }: any) => {
   return (
     <Container>
-      <div>
-        <Head>
-          <meta
-            http-equiv="Content-Security-Policy"
-            content="upgrade-insecure-requests"
-          />
-        </Head>
-      </div>
       <Navbar />
       {children}
       <Footer />
